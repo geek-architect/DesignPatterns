4fe0994b8f0f93bd80805b68f4b03bc09ca21d40
geek-Architect <david_580001@163.com>
2022-12-28 17:08:07 +0800

    中介者模式-案例代码

---
+++ mediator-pattern/src/main/java/com/geekarchitect/patterns/mediator/MediatorPatternApplication.java
@@ -0,0 +1,13 @@
+package com.geekarchitect.patterns.mediator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediatorPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediatorPatternApplication.class, args);
+    }
+
+}
---
+++ mediator-pattern/src/test/java/com/geekarchitect/patterns/mediator/MediatorPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekarchitect.patterns.mediator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediatorPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
